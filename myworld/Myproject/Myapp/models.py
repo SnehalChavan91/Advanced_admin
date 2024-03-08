54af20689cce82f8799adf38d28a109a03e54009
snehal <you@example.com>
2024-03-08 17:24:44 +0530

    changed some fields

--- myworld/Myproject/Myapp/models.py
+++ myworld/Myproject/Myapp/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-
+from datetime import datetime
+from django.utils import timezone
 # Create your models here.
 class Author(models.Model):
-    first_name=models.CharField(max_length=100)
-    last_name=models.CharField(max_length=100)
-    biography=models.TextField
-    date_of_birth=models.DateField()
-    pub_date=models.DateField()
+    name=models.CharField(max_length=100)
+    title=models.CharField(max_length=3,default="Mr")
+    birth_date=models.DateField()
+    pub_date=models.DateField(default=timezone.now())
 
     def __str__(self):
-        return f"{self.first_name}{self.last_name}"
+        return f"{self.name}{self.birth_date}"
     
 
 class Reader(models.Model):
